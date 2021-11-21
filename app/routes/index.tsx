739a7ae93d9490cbe0b4a4261beb9b977d871593
Kent C. Dodds <me+github@kentcdodds.com>
2021-11-21 13:06:30 -0700

    add github link

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -34,6 +34,9 @@ export default function Index() {
               <Link to="jokes">Read Jokes</Link>
             </li>
             <li>
+              <Link to="https://github.com/remix-run/remix-jokes">GitHub</Link>
+            </li>
+            <li>
               <Link reloadDocument to="/jokes.rss">
                 RSS
               </Link>
